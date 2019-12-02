71a82b0a0c434d774a9b72ec5c5e753a9475e722
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-12-02 16:22:11 -0600

    Create intersection-of-rectangles.h

---
+++ intersection-of-rectanges/c-lang/intersection-of-rectangles.h
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+
+typedef struct {
+    int leftX;
+    int bottomY;
+    int width;
+    int height;
+} Rectangle;
+
+Rectangle findRectangularOverlap(Rectangle rect1, Rectangle rect2)
+{
+    // calculate the overlap between the two rectangles
+
+    return rect1;
+}
